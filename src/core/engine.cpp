42dce1c0c1fc4d86a15336b2ae815de333812ea7
vadymchan <vadymchan@gmail.com>
2025-06-22 11:26:17 +0300

    minor fix - change the window name

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -236,7 +236,7 @@ auto Engine::initialize() -> bool {
   // window
   // ------------------------------------------------------------------------
   m_window_ = std::make_unique<Window>(
-      renderingApiString,
+      "arise",
       // Desired size (for maximized window will be 0)
       math::Dimension2i{0, 0},
       math::Point2i{SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED},

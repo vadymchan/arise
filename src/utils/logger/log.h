de250b1b0c6363595881851f388c56a047599db2
vadymchan <vadymchan@gmail.com>
2025-08-06 23:50:37 +0300

    update log utils

--- src/utils/logger/log.h
+++ src/utils/logger/log.h
@@ -7,105 +7,52 @@
 
 namespace arise {
 
-// -----------------------------------------------------------------------------
-// Level: Trace
-// -----------------------------------------------------------------------------
+namespace detail {
+
+inline void Log(LogLevel level, const std::source_location& loc, const std::string& msg) {
+  GlobalLogger::Log(level, msg, loc);
+}
+
+template <typename... Args>
+inline void Log(LogLevel level, const std::source_location& loc, fmt::format_string<Args...> fmtStr, Args&&... args) {
+  GlobalLogger::Log(level, fmt::format(fmtStr, std::forward<Args>(args)...), loc);
+}
+
+}  // namespace detail
 
 inline void LogTrace(const std::string& msg, const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Trace, msg, loc);
+  detail::Log(LogLevel::Trace, loc, msg);
 }
-
-template <typename... Args>
-inline void LogTrace(fmt::format_string<Args...> fmtStr,
-                     Args&&... args,
-                     const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Trace, fmt::format(fmtStr, std::forward<Args>(args)...), loc);
-}
-
-// -----------------------------------------------------------------------------
-// Level: Debug
-// -----------------------------------------------------------------------------
 
 inline void LogDebug(const std::string& msg, const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Debug, msg, loc);
+  detail::Log(LogLevel::Debug, loc, msg);
 }
-
-template <typename... Args>
-inline void LogDebug(fmt::format_string<Args...> fmtStr,
-                     Args&&... args,
-                     const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Debug, fmt::format(fmtStr, std::forward<Args>(args)...), loc);
-}
-
-// -----------------------------------------------------------------------------
-// Level: Info
-// -----------------------------------------------------------------------------
 
 inline void LogInfo(const std::string& msg, const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Info, msg, loc);
+  detail::Log(LogLevel::Info, loc, msg);
 }
-
-template <typename... Args>
-inline void LogInfo(fmt::format_string<Args...> fmtStr,
-                    Args&&... args,
-                    const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Info, fmt::format(fmtStr, std::forward<Args>(args)...), loc);
-}
-
-// -----------------------------------------------------------------------------
-// Level: Warning
-// -----------------------------------------------------------------------------
 
 inline void LogWarn(const std::string& msg, const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Warning, msg, loc);
+  detail::Log(LogLevel::Warning, loc, msg);
 }
-
-template <typename... Args>
-inline void LogWarn(fmt::format_string<Args...> fmtStr,
-                    Args&&... args,
-                    const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Warning, fmt::format(fmtStr, std::forward<Args>(args)...), loc);
-}
-
-// -----------------------------------------------------------------------------
-// Level: Error
-// -----------------------------------------------------------------------------
 
 inline void LogError(const std::string& msg, const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Error, msg, loc);
+  detail::Log(LogLevel::Error, loc, msg);
 }
-
-template <typename... Args>
-inline void LogError(fmt::format_string<Args...> fmtStr,
-                     Args&&... args,
-                     const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Error, fmt::format(fmtStr, std::forward<Args>(args)...), loc);
-}
-
-// -----------------------------------------------------------------------------
-// Level: Fatal
-// -----------------------------------------------------------------------------
 
 inline void LogFatal(const std::string& msg, const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Fatal, msg, loc);
-}
-
-template <typename... Args>
-inline void LogFatal(fmt::format_string<Args...> fmtStr,
-                     Args&&... args,
-                     const std::source_location& loc = std::source_location::current()) {
-  GlobalLogger::Log(LogLevel::Fatal, fmt::format(fmtStr, std::forward<Args>(args)...), loc);
+  detail::Log(LogLevel::Fatal, loc, msg);
 }
 
 }  // namespace arise
 
 #ifndef ARISE_DISABLE_LOGGING
-#define LOG_TRACE(...) arise::LogTrace(__VA_ARGS__)
-#define LOG_DEBUG(...) arise::LogDebug(__VA_ARGS__)
-#define LOG_INFO(...)  arise::LogInfo(__VA_ARGS__)
-#define LOG_WARN(...)  arise::LogWarn(__VA_ARGS__)
-#define LOG_ERROR(...) arise::LogError(__VA_ARGS__)
-#define LOG_FATAL(...) arise::LogFatal(__VA_ARGS__)
+#define LOG_TRACE(...) arise::detail::Log(LogLevel::Trace, std::source_location::current(), __VA_ARGS__)
+#define LOG_DEBUG(...) arise::detail::Log(LogLevel::Debug, std::source_location::current(), __VA_ARGS__)
+#define LOG_INFO(...)  arise::detail::Log(LogLevel::Info, std::source_location::current(), __VA_ARGS__)
+#define LOG_WARN(...)  arise::detail::Log(LogLevel::Warning, std::source_location::current(), __VA_ARGS__)
+#define LOG_ERROR(...) arise::detail::Log(LogLevel::Error, std::source_location::current(), __VA_ARGS__)
+#define LOG_FATAL(...) arise::detail::Log(LogLevel::Fatal, std::source_location::current(), __VA_ARGS__)
 #else
 #define LOG_TRACE(...)
 #define LOG_DEBUG(...)
